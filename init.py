80ba7917161d7f8f12990edace6387af523cf552
Cieciak <nikodemrzasa@wp.pl>
2022-10-22 23:26:07 +0200

    add: Better UX

--- init.py
+++ init.py
@@ -1,28 +1,49 @@
 from math import cos, sin, sqrt, ceil
+import svg, sys
 
-text = r'<svg xmlns="http://www.w3.org/2000/svg" width="400" height="400">'
+# Amount of points to draw
+POINTS = 100_000
 
-def pos_to_place(i):
-    power = 0.51
-    x = i**power * cos(i)
-    y = i**power * sin(i)
+# Try to get function from command
+try: user_function = sys.argv[1]
+except IndexError: user_function = None
+# Try to get filename from command
+try: file_name = sys.argv[2]
+except IndexError: file_name = None
+# Try to get amount from command
+try: points = int(sys.argv[3])
+except IndexError: points = 100_000
+except ValueError:points = 100_000
 
+# Ask user for the function
+if not user_function: user_function = input("Enter f(n): ℕ → X, n ∈ ℕ: ")
+compiled = lambda n: eval(compile(user_function, './err', 'eval'))
+
+# Determine the output filename
+if not file_name: file_name = input("Enter filename: ")
+if not file_name: file_name = f'{user_function}.svg'.replace(' ', '_')
+
+def convert_to_spiral(n: int, *, power: float = 0.5, scale: float = 1) -> tuple[float, float]:
+    x = n ** power * cos(n)
+    y = n ** power * sin(n)
     return x, y
 
-def f(x):
-    return 6 * x + 5
-
 def is_prime(x):
-    for i in range(2, x):
+    for i in range(2, ceil(sqrt(x))):
         if (x % i == 0): return False
     return True
 
-def draw_circle(x, y, i):
-    return f'<circle cx="{200 + x}" cy="{200 + y}" r="1" fill="{"black" if is_prime(i) else "white"}"/>\n'
+output_image = svg.SVG(1000, 1000)
 
-for j in range(10_000):
-    text += draw_circle(*pos_to_place(j), f(j))
+for n in range(0, POINTS):
+    x, y  = convert_to_spiral(n)
 
-text += r'</svg>'
+    mapped = compiled(n)
+    prime = is_prime(mapped)
 
-print(text)
+    point = svg.SVG_Circle(x + 500, y + 500, 1, fill = 'black' if prime else 'white')
+
+    output_image.append(point) 
+    if not (n % 10): print(f'{n/POINTS * 100}%')
+
+output_image.save(file_name)
---
+++ svg.py
@@ -0,0 +1,42 @@
+
+class SVG_Element:
+
+    def __init__(self, *, depth: int = 0, **_) -> None: self.depth = depth
+
+class SVG_Circle(SVG_Element):
+
+    ARGS = ['cx', 'cy', 'r', 'fill']
+    PATTERN = r'<circle cx="_cx_" cy="_cy_" r="_r_" fill="_fill_"/>'
+
+    def __init__(self, cx: float, cy: float, r: float, *, fill: str = 'black', depth: float = 0) -> None:
+        args = locals()
+        args.pop('self')
+        super().__init__(**args)
+        self.passed = locals()
+
+    def __str__(self) -> str:
+        output = self.PATTERN
+        for key in self.ARGS:
+            output = output.replace(f'_{key}_', f'{self.passed[key]}')
+        return output
+
+class SVG:
+
+    def __init__(self, width: int = 500, heigth: int = 500, *, offset: tuple[float, float] = (0,0)) -> None:
+        self.width = width
+        self.heigth = heigth
+
+        self.canvas: list[SVG_Element] = []
+
+    def append(self, obj: SVG_Element): self.canvas.append(obj)
+
+    def save(self, path):
+        text = r'<svg xmlns="http://www.w3.org/2000/svg" width="_width_" height="_heigth_">_object_</svg>'
+        text = text.replace('_width_', f'{self.width}').replace('_heigth_', f'{self.heigth}')
+        
+        content = ''.join([str(obj) for obj in self.canvas])
+
+        text = text.replace('_object_', content)
+
+        with open(path, 'w') as file:
+            file.write(text)
