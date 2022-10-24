b81c72a4bd77953caf4fd4190dca9922c80c1493
Cieciak <nikodemrzasa@wp.pl>
2022-10-24 04:02:16 +0200

    test: html 3

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+var current = 0
+var files = []
+
+async function get_directory(dir_name){
+    const url = `https://api.github.com/repos/Cieciak/UlamMod/git/trees/main`
+    const response = await fetch(url).then(response => response.json())
+    const directory = response.tree.find(node => node.path  === dir_name)
+    if (directory) {
+        const response = await fetch(directory.url).then(resonse => resonse.json())
+        const files = response.tree.map(node => node.path)
+        return files
+    }
+}
+
+function prev(){
+    current = (current - 1) % files.length
+    document.getElementById("output").innerHTML = files[current]
+}
 
 async function init(){
-    const url = `https://api.github.com/repos/Cieciak/UlamMod/git/trees/main`
-    const list = await fetch(url).then(res => res.json())
-    const dir = list.tree.find(node => node.path === 'svg');
-    document.getElementById("output").innerHTML = dir
+    files = await get_directory('svg')
+        document.getElementById("output").innerHTML = files
+    
+    
 }
